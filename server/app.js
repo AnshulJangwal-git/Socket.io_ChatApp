e479df8ac726ebd2c884c27c46880006ab185896
anshul <anshuljangwal006@gmail.com>
2021-09-03 15:23:20 +0530

    chat app complete !!

--- server/app.js
+++ server/app.js
@@ -1,66 +1,57 @@
-//express => express is a framework based on nodeJS
-//nodemon => dev dependency(dependency which is only used during development not in production code) 
-//socket.io => socket implement
+// express => express is a framework based on nodeJS
+// nodemon => dev dependency => dependency which is only used during development not in production code
+// socket.io => socket implemented
 
 const express = require("express");
 const { Server } = require("socket.io");
-
-//server is created
+// server is created !!!
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
 const io = new Server(server);
 
-// app.use(express.json()) ;  
+//app.use(express.json());
 app.use(express.static("public"));
 
+let userList = []; // for online list
 
+// connection event is attached on io
+io.on("connection" , function(socket){
 
-let userList = []; //online list
-
-
-
-
-//connection event is attached on io
-io.on("connection", function (socket) {
-    console.log(socket.id + " Connected !!");
-
-    socket.on("userConnected", function (username) {
-        let userObject = { id: socket.id, username: username };
+    socket.on("userConnected" , function(username){
+        let userObject = { id : socket.id , username : username};
         userList.push(userObject);
-        console.log(userList) ;
 
-        //for self
-        socket.emit("online-list", userList);
+        // for self
+        socket.emit("online-list" , userList);
 
-        //broadcast a message to all other clients except sender
-        socket.broadcast.emit("join", { username, userObject });
-
+        // broadcast a message to all other clients except sender
+        socket.broadcast.emit("join" , userObject);
     })
 
-    socket.on("chat", function (chatObj) {
-        socket.broadcast.emit("chatLeft", chatObj);
+    socket.on("chat" , function(chatObj){
+        socket.broadcast.emit("chatLeft" , chatObj);
     })
 
-    socket.on("disconnect", function () {
-        let leftUserObject;
-        let remainingUsers = userList.filter(function (userObject) {
-            if (userObject.id == socket.id) {
-                leftUserObject = userObject;
+    socket.on("disconnect" , function(){
+        let leftUserObj;
+        let remainingUsers = userList.filter(function(userObj){
+            if(userObj.id == socket.id){
+                leftUserObj = userObj;
                 return false;
             }
             return true;
         })
-
         userList = remainingUsers;
-        socket.broadcast.emit("leave", leftUserObject);
+        socket.broadcast.emit("leave" , leftUserObj);
     })
 })
 
 
+// let port = process.env.PORT || 3000;
 
-//tcp port 5500
-server.listen(5500, function () {
-    console.log("server started at port 5500 !!!");
+// tcp port 5500
+server.listen(5500 , function(){
+    console.log("Server started at port 5500 !!!");
 })
